bb61a3176c1a459b43e7fea7b41fff3d3d0e645b
Enrico Oliveira Rocheti <enricorocheti@gmail.com>
2023-09-13 23:28:04 -0300

    Now running simulations multiple times

--- p2_sim_singal.m
+++ p2_sim_singal.m
@@ -21,16 +21,25 @@ end
 w_time_end = s_pos(end)/v_speed;    % time to the vehicle travel through all sensors
 w_time_end = w_time_end + 0.1;      % adding some seconds as a margin
 
-w_time_res = 0.00001;                      	% time resolution (seconds)
+w_time_res = 0.00001;                           % time resolution (seconds)
 t = (0:w_time_res:w_time_end-w_time_res);       % time vector
+t_size = size(t,2);                             % time vector size
 
-w_f1 = 2;               % first dynamic load frequency (Hz)
+%%w_f1 = 2;               % first dynamic load frequency (Hz)
 w_f1_amp = 15;          % first dynamic load amplitude (Kg)     TODO: real amplitude
-w_f2 = 10;              % second dynamic load frequency (Hz)
+%%w_f2 = 10;              % second dynamic load frequency (Hz)
 w_f2_amp = 10;          % second dynamic load amplitude (Kg)    TODO: real amplitude
 w_static_load = 100;  	% Axle weight                           TODO: real weight
 
-w_signal = w_static_load + w_f1_amp*sin(2*pi*w_f1*t) + w_f2_amp*sin(2*pi*w_f2*t);
+n = 1000; % number of simulations
+
+w_signal = zeros(n,t_size);
+for i = 1:n
+	w_f1 = (rand(1)*(4-1))+1;
+    w_f2 = (rand(1)*(12-8))+8;
+    w_signal(i,:) = w_static_load + w_f1_amp*sin(2*pi*w_f1*t) + w_f2_amp*sin(2*pi*w_f2*t);
+end
+%%w_signal = w_static_load + w_f1_amp*sin(2*pi*w_f1*t) + w_f2_amp*sin(2*pi*w_f2*t);
 
 %% Simulations
 
@@ -43,21 +52,24 @@ end
 % sensor measure index
 s_idx_ini = zeros(1,s_qtty);
 for i = 1:s_qtty
-    s_idx_ini(1,i) = find( abs(t-s_time(i)) < (w_time_res*0.99) );
+    s_idx_ini(i) = find( abs(t-s_time(i)) < (w_time_res*0.99) );
 end
 
 % sensor signal
-t_size = size(t,2);
-s_w_signal = zeros(1,t_size);
+%%s_w_signal = zeros(1,t_size);
+s_w_signal = zeros(n,t_size);
+s_w_avg = zeros(1,n);
 
-for i = 1:s_qtty
-    for j = s_idx_ini(1,i):s_idx_ini(1,i)+1
-        s_w_signal(j) = w_signal(j);
+for k = 1:n
+    for i = 1:s_qtty
+        s_w_signal(k,s_idx_ini(i)) = w_signal(k,s_idx_ini(i));
     end
+    s_w_avg(k) = mean( findpeaks(s_w_signal(k,:)) );
 end
-s_w_measures = findpeaks(s_w_signal);
+%%s_w_measures = findpeaks(s_w_signal);
 
 %% Outputs
+%{
 figure(1);
 plot(t,w_signal,'r');
 hold on
@@ -71,11 +83,27 @@ ylabel('Load [Kg]');
 line ([t(1) t(end)],[w_static_load w_static_load],'linestyle', '--','color', 'g');
 %legend('Dynamic axle load','Sensor measures','Static axle load');
 title("Vehicle speed = " + v_speed*3.6 + " km/h, sensor distance = " + s_dist + " meters");
+%}
 
-s_w_mean = mean(s_w_measures);
-error = (s_w_mean - w_static_load)*100/w_static_load;
+%%s_w_mean = mean(s_w_measures);
+%%error = (s_w_mean - w_static_load)*100/w_static_load;
+
+error = zeros(1,n);
+for i = 1:n
+    error(i) = (s_w_avg(i) - w_static_load)*100/w_static_load;
+end
+
+STR = [' Number of runs = ',num2str(n)];
+disp(STR)
+STR = [' Minimum error = ',num2str(min(error))];
+disp(STR)
+STR = [' Maximum error = ',num2str(max(error))];
+disp(STR)
+STR = [' Mean error = ',num2str(mean(error))];
+disp(STR)
 
 %% Show results
+%{
 STR = [num2str(s_qtty),' sensors, speed = ',num2str(v_speed*3.6),' km/h'];
 disp(STR)
 STR = ['f1 = ',num2str(w_f1),' Hz, f2 = ',num2str(w_f2),' Hz'];
@@ -88,3 +116,4 @@ STR = ['estim. load = ',num2str(s_w_mean),' Kg'];
 disp(STR)
 STR = ['error = ',num2str(error),'%'];
 disp(STR)
+%}
